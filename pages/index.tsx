73d72c7ee1e9f6fae1c7f5e283a452695aa23fc5
k4m4 <nikolaskam@gmail.com>
2022-10-26 04:07:26 -0400

    Break page into components

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,21 @@ import {
   Text,
   Stack,
   Box,
+  BoxProps,
   Image,
   Heading,
+  HeadingProps,
   Code,
+  CodeProps,
   Wrap,
   WrapItem,
   Container,
   UnorderedList,
   ListItem,
   Link,
-  Tabs,
+  LinkProps,
+  Tabs as ChakraTabs,
+  TabsProps as ChakraTabsProps,
   TabList,
   TabPanels,
   Tab,
@@ -21,117 +26,204 @@ import {
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 
-const Home: NextPage = () => {
+const HeadingContainer = () => {
+  return (
+    <Container>
+      <Stack spacing={2}>
+        <Image src="/kevlar.png" alt="Kevlar Logo" h="95px" mx="auto" />
+        <Heading as="h1" size="lg" fontWeight={700}>kevlar</Heading>
+        <Text fontSize="md">Make your wallet trustless.</Text>
+      </Stack>
+    </Container>
+  )
+}
+
+const SubheadingContainer = () => {
+  return (
+    <Container maxW='container.md'>
+      <Text fontSize="sm">
+        Most wallets rely on centralized, trusted full nodes.
+        Kevlar lets you run a light-client-based RPC proxy on your
+        device that you can add to any RPC-based wallet.  Now every RPC call made by your wallet is verified
+        using Merkle Inclusion proofs.
+      </Text>
+    </Container>
+  )
+}
+
+type TabsProps = Omit<ChakraTabsProps, 'colorScheme' | 'align'>
+
+const Tabs = (props: TabsProps) => {
+  return (
+    <ChakraTabs align="center" colorScheme="yellow" {...props} />
+  )
+}
+
+type CodeSnippetProps = Omit<CodeProps, 'bg | rounded'>
+
+const CodeSnippet = (props: CodeSnippetProps) => {
+  return (
+    <Code
+      p={4}
+      bg="gray.100"
+      rounded="lg"
+      {...props}
+    />
+  )
+}
+
+type InstructionHeadingProps = Omit<HeadingProps, 'as' | 'fontSize' | 'fontWeight' | 'textTransform'>
+
+const InstructionHeading = (props: InstructionHeadingProps) => {
+  return (
+    <Heading
+      as="h2"
+      fontSize="xs"
+      fontWeight="700"
+      textTransform="uppercase"
+      mb={2}
+      {...props}
+    />
+  )
+}
+
+type InstructionProps = BoxProps & {
+  number: number
+  title: string
+}
+
+const Instruction = ({ number, title, children, ...props }: InstructionProps) => {
+  return (
+    <WrapItem flex="1">
+      <Box w="100%" {...props}>
+        <InstructionHeading>
+          {number}. {title}
+        </InstructionHeading>
+        {children}
+      </Box>
+    </WrapItem>
+  )
+}
+
+const InstructionsContainer = () => {
   const [chainIdTabIndex, setChainIdTabIndex] = React.useState<0 | 1>(0)
   return (
-    <Stack display="flex" alignItems="center" mx={3} mb={10} spacing={9} textAlign="center" mt="90px">
-      <Container maxW='container.md'>
-        <Stack spacing={2}>
-          <Image src="/kevlar.png" alt="Kevlar Logo" h="95px" mx="auto" />
-          <Heading as="h1" size="lg" fontWeight={700}>kevlar</Heading>
-          <Text fontSize="md">Turn your wallet trustless.</Text>
-        </Stack>
-      </Container>
-      <Container maxW='container.sm'>
-        <Text fontSize="sm">
-          Most wallets rely on centralized, trusted full nodes.
-          Kevlar lets you run a light-client-based RPC proxy on your
-          device that you can add to any RPC-based wallet.  Now every RPC call made by your wallet is verified
-          using Merkle Inclusion proofs.
-        </Text>
-      </Container>
-      <Container maxW='container.lg'>
-        <Wrap spacing={4}>
-          <WrapItem flex="1">
-            <Box w="100%">
-              <Heading as="h2" fontSize="xs" fontWeight="700" textTransform="uppercase" mb={2}>
-                1. Install Kevlar
-              </Heading>
-              <Tabs align="center" colorScheme="yellow">
-                <TabList>
-                  <Tab>npm</Tab>
-                  <Tab>yarn</Tab>
-                </TabList>
-                <TabPanels>
-                  <TabPanel>
-                    <Code p={4} bg="gray.100" rounded="lg">
-                      npm install -g @lightclients/kevlar
-                    </Code>
-                  </TabPanel>
-                  <TabPanel>
-                    <Code p={4} bg="gray.100" rounded="lg">
-                      yarn global add @lightclients/kevlar
-                    </Code>
-                  </TabPanel>
-                </TabPanels>
-              </Tabs>
-            </Box>
-          </WrapItem>
-          <WrapItem flex="1">
-            <Box w="100%">
-              <Heading as="h2" fontSize="xs" fontWeight="700" textTransform="uppercase" mb={2}>
-                2. Start RPC proxy
-              </Heading>
-              <Tabs align="center" onChange={(index) => setChainIdTabIndex(index as 0 | 1)} colorScheme="yellow">
-                <TabList>
-                  <Tab>mainnet</Tab>
-                  <Tab>goerli</Tab>
-                </TabList>
-                <TabPanels>
-                  <TabPanel>
-                    <Code p={4} bg="gray.100" rounded="lg">
-                      kevlar -n 1
-                    </Code>
-                  </TabPanel>
-                  <TabPanel>
-                    <Code p={4} bg="gray.100" rounded="lg">
-                      kevlar -n 5
-                    </Code>
-                  </TabPanel>
-                </TabPanels>
-              </Tabs>
-            </Box>
-          </WrapItem>
-          <WrapItem flex="1">
-            <Box w="100%">
-              <Heading as="h2" fontSize="xs" fontWeight="700" textTransform="uppercase" mb={2}>
-                3. Add RPC to Metamask
-              </Heading>
-              <Box fontSize="sm">
-                <Text>On your Metamask extension:</Text>
-                <Wrap align="center" justify="center" spacing={1} mt={2}>
-                  <WrapItem display="flex" alignItems="center">
-                    <Text>Network Selector dropdown</Text>
-                    <ChevronRightIcon ml={1} color="gray.500" />
-                  </WrapItem>
-                  <WrapItem display="flex" alignItems="center">
-                    <Text as="span" fontStyle="italic">Add network</Text>
-                    <ChevronRightIcon ml={1} color="gray.500" />
-                  </WrapItem>
-                  <WrapItem display="flex" alignItems="center">
-                    <Box>
-                      <Text as="span" fontStyle="italic">Chain ID</Text>: <Code p={2} bg="gray.100" rounded="lg">{chainIdTabIndex === 0 ? 1 : 5}</Code>
-                    </Box>
-                    <ChevronRightIcon ml={1} color="gray.500" />
-                  </WrapItem>
-                  <WrapItem display="flex" alignItems="center">
-                    <Box>
-                      <Text as="span" fontStyle="italic">New RPC URL</Text>: <Code p={2} bg="gray.100" rounded="lg">{'http://localhost:8546'}</Code>
-                    </Box>
-                  </WrapItem>
-                </Wrap>
-              </Box>
-            </Box>
-          </WrapItem>
-        </Wrap>
-      </Container>
-      <Container maxW='container.md' as="footer" display="flex" alignItems="center" justifyContent="center">
-        <UnorderedList fontSize="sm" listStyleType="none" p={0} display="flex" justifyContent="center" alignItems="center">
-          <ListItem color="yellow.500"><Link href="https://arxiv.org/pdf/2209.08673.pdf" isExternal>Paper</Link></ListItem>
-          <ListItem color="yellow.500" mx={[6, 7, 8]}><Link href="https://twitter.com/shresth3103/status/1581370823083839488" isExternal>How does it work?</Link></ListItem>
-          <ListItem color="yellow.500"><Link href="https://github.com/lightclients/kevlar" isExternal>GitHub</Link></ListItem>
-        </UnorderedList>
-      </Container>
+    <Container maxW='container.md'>
+      <Wrap spacing={4}>
+        <Instruction number={1} title="Install Kevlar">
+          <Tabs>
+            <TabList>
+              <Tab>npm</Tab>
+              <Tab>yarn</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <CodeSnippet>
+                  npm install -g @lightclients/kevlar
+                </CodeSnippet>
+              </TabPanel>
+              <TabPanel>
+                <CodeSnippet>
+                  yarn global add @lightclients/kevlar
+                </CodeSnippet>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Instruction>
+        <Instruction number={2} title="Start RPC proxy">
+          <Tabs onChange={(index) => setChainIdTabIndex(index as 0 | 1)}>
+            <TabList>
+              <Tab>mainnet</Tab>
+              <Tab>goerli</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <CodeSnippet>
+                  kevlar -n 1
+                </CodeSnippet>
+              </TabPanel>
+              <TabPanel>
+                <CodeSnippet>
+                  kevlar -n 5
+                </CodeSnippet>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Instruction>
+        <Instruction number={3} title="Add RPC to Metamask">
+          <Box fontSize="sm">
+            <Text>On your Metamask extension:</Text>
+            <Wrap align="center" justify="center" spacing={1} mt={2}>
+              <WrapItem display="flex" alignItems="center">
+                <Text>Network Selector dropdown</Text>
+                <ChevronRightIcon ml={1} color="gray.500" />
+              </WrapItem>
+              <WrapItem display="flex" alignItems="center">
+                <Text as="span" fontStyle="italic">Add network</Text>
+                <ChevronRightIcon ml={1} color="gray.500" />
+              </WrapItem>
+              <WrapItem display="flex" alignItems="center">
+                <Box>
+                  <Text as="span" fontStyle="italic">Chain ID</Text>: <CodeSnippet p={2}>{chainIdTabIndex === 0 ? 1 : 5}</CodeSnippet>
+                </Box>
+                <ChevronRightIcon ml={1} color="gray.500" />
+              </WrapItem>
+              <WrapItem display="flex" alignItems="center">
+                <Box>
+                  <Text as="span" fontStyle="italic">New RPC URL</Text>: <CodeSnippet p={2}>{'http://localhost:8546'}</CodeSnippet>
+                </Box>
+              </WrapItem>
+            </Wrap>
+          </Box>
+        </Instruction>
+      </Wrap>
+    </Container>
+  )
+}
+
+type FooterLinkProps = Omit<LinkProps, 'isExternal'>
+
+const FooterLink = (props: FooterLinkProps) => {
+  return (
+    <ListItem mx={[6, 7, 8]} color="yellow.500">
+      <Link isExternal {...props} />
+    </ListItem>
+  )
+}
+
+const FooterContainer = () => {
+  return (
+    <Container as="footer" display="flex" alignItems="center" justifyContent="center">
+      <UnorderedList fontSize="sm" listStyleType="none" p={0} display="flex" justifyContent="center" alignItems="center">
+        <FooterLink href="https://arxiv.org/pdf/2209.08673.pdf">
+          Paper
+        </FooterLink>
+        <FooterLink href="https://twitter.com/shresth3103/status/1581370823083839488">
+          How does it work?
+        </FooterLink>
+        <FooterLink href="https://github.com/lightclients/kevlar">
+          GitHub
+        </FooterLink>
+      </UnorderedList>
+    </Container>
+  )
+}
+
+const Home: NextPage = () => {
+  return (
+    <Stack
+      display="flex"
+      alignItems="center"
+      mx={3}
+      mb={10}
+      spacing={10}
+      textAlign="center"
+      mt="90px"
+    >
+      <HeadingContainer />
+      <SubheadingContainer />
+      <InstructionsContainer />
+      <FooterContainer />
     </Stack>
   )
 }
