346f82292d3c05129fdb0f5a2d730054bd2cbfec
k4m4 <nikolaskam@gmail.com>
2022-10-26 04:15:33 -0400

    Update subheading

--- pages/index.tsx
+++ pages/index.tsx
@@ -44,7 +44,8 @@ const SubheadingContainer = () => {
       <Text fontSize="sm">
         Most wallets rely on centralized, trusted full nodes.
         Kevlar lets you run a light-client-based RPC proxy on your
-        device that you can add to any RPC-based wallet.  Now every RPC call made by your wallet is verified
+        device that you can add to any RPC-based wallet.
+        With Kevlar, every RPC call made by your wallet is verified
         using Merkle Inclusion proofs.
       </Text>
     </Container>
