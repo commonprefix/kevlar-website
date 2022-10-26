f7ba251752cf6a46ff6c7405b7490b9e6604b7c2
k4m4 <nikolaskam@gmail.com>
2022-10-26 03:54:11 -0400

    Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,6 +39,11 @@ const App = ({ Component, pageProps }: AppProps) => {
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
         <meta name="description" content="Kevlar makes Metamask, or any RPC-based wallet, completely trustless." />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="shortcut icon" type="image/png" href="/kevlar.png" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
       </Head>
       <DefaultSeo {...SEO} />
       <ChakraProvider theme={theme}>
